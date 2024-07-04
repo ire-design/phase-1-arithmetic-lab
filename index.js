f35f3d268ea045dc044238f18d6709b6aeed0599
ire-design <irenemwikalii04@gmail.com>
2024-07-04 08:22:37 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2
+
+
+let random = Math.floor((Math.random()) * 10);
+    
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4
+
+let myArr = [10, 16, 3, 17, 11, 18, 20]
+let max = Math.max(...myArr)//Math.max does not accept an array directly
